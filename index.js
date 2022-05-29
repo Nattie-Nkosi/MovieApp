7390dcd415dc24aca0c1448eb7f9b6ccc584c442
Nutty4616 <nkosin361@gmail.com>
2022-05-29 19:03:27 +0200

    Fixed some bugs

--- index.js
+++ index.js
@@ -1,108 +1,113 @@
 const autocompleteConfig = {
-    renderOption: (movie) => {
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-        return `
+  renderOption: (movie) => {
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+    return `
         <img src="${imgSrc}" />
         ${movie.Title} (${movie.Year})
-    `
-    },
-    inputValue: (movie) => {
-        return movie.Title;
-    },
-    async fetchData(seachTerm) {
-        const response = await axios.get('https://www.omdbapi.com/', {
-            params: {
-                apikey: 'c326d45c',
-                s: seachTerm
-            }
-        });
-        
-        if(response.data.Error){
-            return [];
-        }
-        
-         return response.data.Search;       
+    `;
+  },
+  inputValue: (movie) => {
+    return movie.Title;
+  },
+  async fetchData(seachTerm) {
+    const response = await axios.get("https://www.omdbapi.com/", {
+      params: {
+        apikey: "c326d45c",
+        s: seachTerm,
+      },
+    });
+
+    if (response.data.Error) {
+      return [];
     }
+
+    return response.data.Search;
+  },
 };
 
 createAutoComplete({
-    // The dots means take all the properties of autocompleteConfig to this function
-    ...autocompleteConfig,
-    root: document.querySelector('#left-autocomplete'),
-    onOptionSelect: (movie) => {
-        document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#left-summery'), 'left');
-    } 
+  // The dots means take all the properties of autocompleteConfig to this function
+  ...autocompleteConfig,
+  root: document.querySelector("#left-autocomplete"),
+  onOptionSelect: (movie) => {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#left-summery"), "left");
+  },
 });
 createAutoComplete({
-    ...autocompleteConfig,
-    root: document.querySelector('#right-autocomplete'),
-    onOptionSelect: (movie) => {
-        document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#right-summery'), 'right');
-    } 
+  ...autocompleteConfig,
+  root: document.querySelector("#right-autocomplete"),
+  onOptionSelect: (movie) => {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#right-summery"), "right");
+  },
 });
 
 let leftMovie;
 let rightMovie;
-const onMovieSelect = async (movie,summeryElement, side) =>{
-    const response = await axios.get('https://www.omdbapi.com/', {
-        params: {
-            apikey: 'c326d45c',
-            i: movie.imdbID
-        }
-    });
+const onMovieSelect = async (movie, summeryElement, side) => {
+  const response = await axios.get("https://www.omdbapi.com/", {
+    params: {
+      apikey: "c326d45c",
+      i: movie.imdbID,
+    },
+  });
 
-    summeryElement.innerHTML = movieTemplate(response.data);
+  summeryElement.innerHTML = movieTemplate(response.data);
 
-    if(side === 'left'){
-        leftMovie = response.data;
-    }else {
-        rightMovie = response.data
-    }
+  if (side === "left") {
+    leftMovie = response.data;
+  } else {
+    rightMovie = response.data;
+  }
 
-    if (leftMovie && rightMovie) {
-        runComperison(); 
-    }
-
+  if (leftMovie && rightMovie) {
+    runComperison();
+  }
 };
 
 const runComperison = () => {
-    const leftSideStats = document.querySelectorAll('#left-summery .notification');
-    const rightSideStats = document.querySelectorAll('#right-summery .notification');
+  const leftSideStats = document.querySelectorAll(
+    "#left-summery .notification"
+  );
+  const rightSideStats = document.querySelectorAll(
+    "#right-summery .notification"
+  );
 
-    leftSideStats.forEach((leftStat, index) => {
-        const rightStat = rightSideStats[index];
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
 
-        const leftSideValue = leftStat.dataset.value;
-        const rightSideValue = rightStat.dataset.value;
+    const leftSideValue = parseInt(leftStat.dataset.value);
+    const rightSideValue = parseInt(rightStat.dataset.value);
 
-        if (rightSideValue > leftSideValue) {
-            leftStat.classList.remove('is-primary');
-            leftStat.classList.add('is-warning');
-        } else {
-            rightStat.classList.remove('is-primary');
-            rightStat.classList.add('is-warning');
-        }
-    });
+    if (rightSideValue > leftSideValue) {
+      leftStat.classList.remove("is-primary");
+      leftStat.classList.add("is-warning");
+    } else {
+      rightStat.classList.remove("is-primary");
+      rightStat.classList.add("is-warning");
+    }
+  });
 };
 
 const movieTemplate = (movieDetail) => {
-    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
-    const metascore = parseInt(movieDetail.Metascore);
-    const imbdRating = parseFloat(movieDetail.imdbRating);
-    const imbdVotes = parseInt((movieDetail.imdbVotes).replace(/,/g, '')); 
+  const dollars = parseInt(
+    movieDetail.BoxOffice.replace(/\$/g, "").replace(/,/g, "")
+  );
+  const metascore = parseInt(movieDetail.Metascore);
+  const imbdRating = parseFloat(movieDetail.imdbRating);
+  const imbdVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ""));
 
-    const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
-        const value = parseInt(word);
+  const awards = movieDetail.Awards.split(" ").reduce((prev, word) => {
+    const value = parseInt(word);
 
-        if (isNaN(value)){
-            return;
-        } else {
-            return prev + value;
-        }
-    }, 0);
-    return `
+    if (isNaN(value)) {
+      return;
+    } else {
+      return prev + value;
+    }
+  }, 0);
+  return `
         <article class="media">
             <figure class="media-left">
                 <p class="image">
